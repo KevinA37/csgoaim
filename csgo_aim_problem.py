1d034163a75d61fd5fe5cbf347265dddf3e6857d
KevinA37 <104632210+KevinA37@users.noreply.github.com>
2023-10-18 17:40:42 +0200

    Update csgo_aim_problem.py

--- csgo_aim_problem.py
+++ csgo_aim_problem.py
@@ -4,33 +4,36 @@ import cv2 as cv
 import numpy as np
 import win32gui
 import win32api
-import keyboard
 import time
 import pydirectinput as pdi
+import keyboard
 
 # Model without specifying classes
-model = torch.hub.load("ultralytics/yolov5", "yolov5s")
+model = torch.hub.load("ultralytics/yolov5", "yolov5n")
 
 # Confidence threshold
 confidence_threshold = 0.4
 
-# Define the bounding box
-bounding_box = {'top': 340, 'left': 800, 'width': 350, 'height': 400}
-
-# Obtain the window handle (HWND) for the target window
-def find_window(title):
-    hwnd = win32gui.FindWindow(None, title)
-    return hwnd
-
+# Define the window title of the target application
 target_window_title = "Counter-Strike 2"  # Replace with the actual window title
-target_window_handle = find_window(target_window_title)
 
 sct = mss()
 
+frame_count = 0
+start_time = time.time()
+
 while True:
-    # Check if the active window matches the target window
-    active_window_handle = win32gui.GetForegroundWindow()
-    if active_window_handle == target_window_handle:
+    # Find the target window by its title
+    target_window_handle = win32gui.FindWindow(None, target_window_title)
+
+    if target_window_handle:
+        # Get the screen coordinates of the target window
+        left, top, right, bottom = win32gui.GetWindowRect(target_window_handle)
+
+        # Define the bounding box using the window coordinates
+        bounding_box = {'left': left, 'top': top, 'width': right - left, 'height': bottom - top}
+
+        # Capture the region of the target window
         sct_img = sct.grab(bounding_box)
         img = np.array(sct_img)
 
@@ -51,21 +54,23 @@ while True:
                 center_y = int((y1 + y2) / 2)
 
                 # Calculate screen coordinates
-                screen_x = bounding_box['left'] + center_x
-                screen_y = bounding_box['top'] + center_y
+                screen_x = left + center_x
+                screen_y = top + center_y
 
                 # Define the number of steps for smoother movement
-                steps = 3
-                scale_factor = 1.7
+                steps = 2  # Increase the number of steps for smoother movement
 
                 # Calculate the delta for each step
-                delta_x = int((screen_x - win32api.GetCursorPos()[0]) / steps)
-                delta_y = int((screen_y - win32api.GetCursorPos()[1]) / steps)
+                delta_x = screen_x - win32api.GetCursorPos()[0]
+                delta_y = screen_y - win32api.GetCursorPos()[1]
 
                 # Perform smooth mouse movement
                 for step in range(steps):
-                    pdi.moveTo(win32api.GetCursorPos()[0] + delta_x, win32api.GetCursorPos()[1] + delta_y)
-                    time.sleep(0.01)  # Adjust the sleep duration for smoother movement
+                    if keyboard.is_pressed('1'):
+                        new_x = int(win32api.GetCursorPos()[0] + delta_x / steps)
+                        new_y = int(win32api.GetCursorPos()[1] + delta_y / steps)
+                        pdi.moveTo(new_x, new_y)
+                        time.sleep(0.1)  # Adjust the sleep duration for smoother movement
 
         # Display the annotated image
         cv.imshow('Screenshot', annotated_image)
@@ -73,3 +78,13 @@ while True:
     if (cv.waitKey(1) & 0xFF) == ord('q'):
         cv.destroyAllWindows()
         break
+
+    frame_count += 1
+    current_time = time.time()
+    elapsed_time = current_time - start_time
+
+    if elapsed_time >= 1.0:
+        fps = frame_count / elapsed_time
+        print(f"FPS: {fps:.2f}")
+        frame_count = 0
+        start_time = current_time
